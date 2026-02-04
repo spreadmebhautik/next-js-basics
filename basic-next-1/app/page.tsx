851d21dc055e412b93ccb2c1e86d7251440831a1
bhautik <spreadmedigitalbhautik@gmail.com>
2026-02-04 19:06:18 +0530

    first commit

---
+++ basic-next-1/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-black bg-orange-500 text-center py-4">Hello World !!</h1>
+      <h2>Bhautik</h2>
+    </>
+  );
+}
