cdbeb8ee9025bc2f0f70f178776957ed09001adc
bhautik <spreadmedigitalbhautik@gmail.com>
2026-02-04 19:26:13 +0530

    test commit

--- basic-next-1/app/page.tsx
+++ basic-next-1/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold text-black bg-orange-500 text-center py-4">Hello World !!</h1>
-      <h2>Bhautik</h2>
     </>
   );
 }
