997d9436e0ea2a1d9a41184adc81e6ffa42af71b
bhautik <spreadmedigitalbhautik@gmail.com>
2026-02-05 12:11:57 +0530

    test commit 1

--- basic-next-1/app/page.tsx
+++ basic-next-1/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold text-black bg-orange-500 text-center py-4">Hello World !!</h1>
+      <h2>Hello NextJs !!</h2>
     </>
   );
 }
